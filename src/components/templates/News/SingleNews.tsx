44fb0df7392f2e1f7172b175f49265933b8bdbbe
Lucas Coutinho <lcscou@gmail.com>
2025-10-28 09:41:53 -0300

    update

--- src/components/templates/News/SingleNews.tsx
+++ src/components/templates/News/SingleNews.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import { GetNewsQuery } from '@/graphql/generated/graphql'
+import { Container } from '@mantine/core'
 import PageTitleBar from '../../ui/PageTitleBar/PageTitleBar'
 
 export default function SingleNews({ data }: { data: GetNewsQuery }) {
@@ -13,7 +14,9 @@ export default function SingleNews({ data }: { data: GetNewsQuery }) {
         readingTime={data.news?.seo?.readingTime}
         author={`${data.news?.author?.node.firstName} ${data.news?.author?.node.lastName}`}
       />
-      <div dangerouslySetInnerHTML={{ __html: data.news?.content || '' }}></div>
+      <Container size="lg" className="my-20">
+        <div dangerouslySetInnerHTML={{ __html: data.news?.content || '' }}></div>
+      </Container>
     </>
   )
 }
