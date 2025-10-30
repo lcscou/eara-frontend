02f526c31ba8b0929ea02c2357f708775b487aa1
Lucas Coutinho <lcscou@gmail.com>
2025-10-30 17:07:37 -0300

    update

--- src/components/sections/FeaturedNews/FeaturedNews.tsx
+++ src/components/sections/FeaturedNews/FeaturedNews.tsx
@@ -5,25 +5,26 @@ import NewsCard from '@/components/ui/NewsCard/NewsCard'
 import Section from '@/components/ui/Section/Section'
 import { GetAllNewsDocument } from '@/graphql/generated/graphql'
 import { cleanHTMLTAG, formatAuthorName } from '@/lib/utils'
-import { useQuery } from '@apollo/client/react'
+import { useSuspenseQuery } from '@apollo/client/react'
 import { Center, Skeleton } from '@mantine/core'
+import { Suspense } from 'react'
 
 export default function FeaturedNews() {
-  const { data, loading } = useQuery(GetAllNewsDocument, {
+  const { data } = useSuspenseQuery(GetAllNewsDocument, {
     variables: { first: 3 },
     fetchPolicy: 'cache-and-network',
   })
-  if (loading) {
-    return (
-      <Section variant="news-grid" subtitle="NEWS" title="Latest research news" containerSize="lg">
-        <Skeleton animate height={500}></Skeleton>
-      </Section>
-    )
-  }
+  // if (loading) {
+  //   return (
+  //     <Section variant="news-grid" subtitle="NEWS" title="Latest research news" containerSize="lg">
+  //       <Skeleton animate height={500}></Skeleton>
+  //     </Section>
+  //   )
+  // }
 
   const featuredNews = data?.allNews?.nodes || []
   return (
-    <>
+    <Suspense fallback={<Skeleton animate height={500} />}>
       <Section variant="news-grid" subtitle="NEWS" title="Latest research news" containerSize="lg">
         <div className="flex flex-col">
           <div className="grid gap-8 sm:grid-cols-4 sm:grid-rows-4">
@@ -66,6 +67,6 @@ export default function FeaturedNews() {
           </Center>
         </div>
       </Section>
-    </>
+    </Suspense>
   )
 }
