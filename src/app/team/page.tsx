6200b5ce59c133e7dce1e3d8fd4ae147d98da510
Lucas Coutinho <lcscou@gmail.com>
2025-12-02 16:35:24 -0300

    update

--- src/app/team/page.tsx
+++ src/app/team/page.tsx
@@ -1,12 +1,7 @@
 import ArchiveTeam from '@/components/templates/Team/ArchiveTeam'
 import PageTitleBar from '@/components/ui/PageTitleBar/PageTitleBar'
-import { GetAllMembersDocument } from '@/graphql/generated/graphql'
-import { query } from '@/lib/apollo-client'
-import { notFound } from 'next/navigation'
 
-export default async function Team() {
-  const { data } = await query({ query: GetAllMembersDocument })
-  if (!data) notFound()
+export default function Team() {
   return (
     <>
       <PageTitleBar title="EARA Members" subtitle="members" />
