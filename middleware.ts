12b7ea19f9101d720fdcf21626785563275c63f8
Lucas Coutinho <lcscou@gmail.com>
2026-02-10 19:23:16 -0300

    eara auth - dev 4

--- middleware.ts
+++
@@ -1,3 +0,0 @@
-// Middleware removed - authentication is now handled at page level
-// Protected pages use validateAuthToken() to ensure token validity
-// See: /src/app/membership/members-area/page.tsx and [...uri]/page.tsx
