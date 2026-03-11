8df0e2e51853cb352c758cd962d032d917b84f1c
Lucas Coutinho <lcscou@gmail.com>
2026-03-11 11:56:22 -0300

    update

--- src/app/team/page.tsx
+++ src/app/team/page.tsx
@@ -2,14 +2,14 @@ import ArchiveTeam from '@/components/templates/Team/ArchiveTeam'
 import PageTitleBar from '@/components/ui/PageTitleBar/PageTitleBar'
 import { Metadata } from 'next'
 export const metadata: Metadata = {
-  title: 'EARA | Team and Delegates',
+  title: 'EARA | Team & Delegates',
   description: 'Learn more about the team and delegates of EARA.',
 }
 
 export default function Team() {
   return (
     <>
-      <PageTitleBar title="Team and Delegates" subtitle="ABOUT EARA" />
+      <PageTitleBar title="Team & Delegates" subtitle="ABOUT EARA" />
       <main>
         <ArchiveTeam />
       </main>
