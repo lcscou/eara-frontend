d31a34ddfb6b22be8b9b07397fabfa30077cec99
Lucas Coutinho <lcscou@gmail.com>
2026-03-18 08:43:49 -0300

    add new image remote pattern

--- next.config.ts
+++ next.config.ts
@@ -7,6 +7,7 @@ const nextConfig: NextConfig = {
       new URL('https://eara.x00.online/**'),
       new URL('http://eara.local/**'),
       new URL('http://raw.githubusercontent.com'),
+      new URL('https://mediumorchid-capybara-348184.hostingersite.com/**'),
     ],
   },
 }
