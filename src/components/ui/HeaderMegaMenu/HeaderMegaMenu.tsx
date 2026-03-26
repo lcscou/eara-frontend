ded1a67967ce2efebb6aab9231497f9da7e942e2
Lucas Coutinho <lcscou@gmail.com>
2026-03-26 09:59:48 -0300

    update

--- src/components/ui/HeaderMegaMenu/HeaderMegaMenu.tsx
+++ src/components/ui/HeaderMegaMenu/HeaderMegaMenu.tsx
@@ -7,6 +7,7 @@ import { IconChevronDown } from '@tabler/icons-react'
 import Image from 'next/image'
 import Link from 'next/link'
 import { usePathname } from 'next/navigation'
+import Search from '../Search/Search'
 
 const ACTIVE_COLOR = '#8fbf29'
 
@@ -98,7 +99,7 @@ export default function HeaderMegaMenu({ data }: HeaderMegaMenuProps) {
                   ) : (
                     <Burger opened={opened} onClick={toggle} aria-label="Toggle navigation" />
                   )}
-                  {/* <Search /> */}
+                  <Search />
                 </Group>
               </div>
             </div>
