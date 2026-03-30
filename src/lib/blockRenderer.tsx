b25735793eb8c488c130644be084a36763c9a81f
Lucas Coutinho <lcscou@gmail.com>
2026-03-30 15:26:14 -0300

    update

--- src/lib/blockRenderer.tsx
+++ src/lib/blockRenderer.tsx
@@ -3474,7 +3474,10 @@ function renderBlock(block: Block, index: number, freeformContent?: string): Rea
       const title = attributes_join?.title || 'Join EARA'
       const description = attributes_join?.description
       const buttonLabel = attributes_join?.buttonLabel || 'Join Now'
-      const submitUrl = 'http://eara.local/wp-json/eara/v1/send-form'
+      const submitUrl = process.env.NEXT_PUBLIC_WORDPRESS_GRAPHQL_ENDPOINT?.replace(
+        '/graphql',
+        '/eara/v1/send-form'
+      )
       const renderMode = attributes_join?.renderMode === 'modal' ? 'modal' : 'inline'
 
       return (
